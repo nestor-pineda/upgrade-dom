77b664ea754cadd47b207db95c9085876c46e773
Nestor Pineda <nestorpinedaelvira@gmail.com>
2021-10-26 11:25:42 +0200

    Iteration#1

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+// #1: Interacción con el DOM
+console.log(document.querySelector(".showme"));
+console.log(document.querySelector("h1#pillado"));
+console.log(document.querySelectorAll("p"));
+console.log(document.querySelectorAll(".pokemon"));
+console.log(document.querySelectorAll('[data-function="testMe"]'));
+console.log(document.querySelectorAll('[data-function="testMe"]')[2]);
